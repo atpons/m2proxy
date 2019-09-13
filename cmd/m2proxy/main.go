b8538699f693747175585609e3b9b4b5bd506e32
atpons <taiga@atpons.com>
2019-09-14 02:19:23 +0900

    Initial commit

---
+++ cmd/m2proxy/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"strconv"
+
+	"github.com/atpons/m2proxy/pkg/util"
+
+	"github.com/atpons/m2proxy/pkg/server"
+	"github.com/atpons/m2proxy/pkg/storage"
+)
+
+func init() {
+	if v, _ := strconv.Atoi(os.Getenv("DEBUG")); v > 0 {
+		util.Debug = v
+	}
+}
+
+func main() {
+	if util.Debug > 0 {
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+	st := storage.NewLocalStorage("sample.db")
+	s := server.NewServer(":11211", &st)
+	s.ListenAndServe()
+}
